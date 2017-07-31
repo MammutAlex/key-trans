9bbe3ca8902798aa265621b4e42465cbe77350c8
Alex <alexk.mammut@gmail.com>
2017-07-31 20:07:39 +0300

    Create base translate in API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -22,7 +22,17 @@ class ApiController extends Controller
             'to' => ['required', 'string'],
             'text' => ['required', 'string'],
         ]);
-        $text = $request->text;
+        $str_search = [
+            "й","ц","у","к","е","н","г","ш","щ","з","х","ъ",
+            "ф","ы","в","а","п","р","о","л","д","ж","э",
+            "я","ч","с","м","и","т","ь","б","ю"
+        ];
+        $str_replace = [
+            "q","w","e","r","t","y","u","i","o","p","[","]",
+            "a","s","d","f","g","h","j","k","l",";","'",
+            "z","x","c","v","b","n","m",",","."
+        ];
+        $text = str_replace($str_search, $str_replace, $request->text);
         return $this->respond(['text' => $text]);
     }
 }
