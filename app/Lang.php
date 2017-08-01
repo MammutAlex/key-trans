087cfe83becef70efff58ee36415851ab3398114
Alex <alexk.mammut@gmail.com>
2017-08-01 09:43:28 +0300

    Create trans class

---
+++ app/Lang.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mammut
+ * Date: 01.08.17
+ * Time: 9:24
+ */
+
+namespace App;
+
+
+class Lang
+{
+    /**
+     * @param string $lang
+     * @return array
+     */
+    public static function get(string $lang): array
+    {
+        return static::$$lang;
+    }
+
+    private static $ru = [
+        "й",
+        "ц",
+        "у",
+        "к",
+        "е",
+        "н",
+        "г",
+        "ш",
+        "щ",
+        "з",
+        "х",
+        "ъ",
+        "ф",
+        "ы",
+        "в",
+        "а",
+        "п",
+        "р",
+        "о",
+        "л",
+        "д",
+        "ж",
+        "э",
+        "я",
+        "ч",
+        "с",
+        "м",
+        "и",
+        "т",
+        "ь",
+        "б",
+        "ю"
+    ];
+    private static $en = [
+        "q",
+        "w",
+        "e",
+        "r",
+        "t",
+        "y",
+        "u",
+        "i",
+        "o",
+        "p",
+        "[",
+        "]",
+        "a",
+        "s",
+        "d",
+        "f",
+        "g",
+        "h",
+        "j",
+        "k",
+        "l",
+        ";",
+        "'",
+        "z",
+        "x",
+        "c",
+        "v",
+        "b",
+        "n",
+        "m",
+        ",",
+        "."
+    ];
+}
